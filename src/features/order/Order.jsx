7cf62d0519392503205ffd73cda6fef56d6d7abb
Devansh27112001 <dkansara2711@gmail.com>
2024-06-17 10:48:22 -0400

    Error handling using useActionData hook

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,47 +1,15 @@
 // Test ID: IIDSAT
 
+import { useLoaderData } from "react-router-dom";
+import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utilities/helpers";
 
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
-
 function Order() {
+  const order = useLoaderData();
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -60,8 +28,8 @@ function Order() {
         <h2>Status</h2>
 
         <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+          {priority && <span>Priority:</span>}
+          <span> {status} order</span>
         </div>
       </div>
 
@@ -81,6 +49,12 @@ function Order() {
       </div>
     </div>
   );
+}
+
+export async function loader({ params }) {
+  // As we know that the params in the URL can be obtained by using the useParams() hook. However, we cannot use a hook here as this is not a component but a function. But to our advantage, React-router has already thought about this situation and has provided us with the params props.
+  const order = await getOrder(params.orderId);
+  return order;
 }
 
 export default Order;

5146afda0843c503a373e2d931d8127152ea2573
Devansh27112001 <dkansara2711@gmail.com>
2024-06-19 03:59:41 -0400

    Styling components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,21 +1,24 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview";
-import Header from "./Header";
-import Loader from "./Loader";
+import { Outlet, useNavigation } from 'react-router-dom'
+import CartOverview from '../features/cart/CartOverview'
+import Header from './Header'
+import Loader from './Loader'
 function AppLayout() {
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
-  return (
-    <div className="layout">
-      {isLoading && <Loader />}
-      <Header />
-      <main>
-        <Outlet />
-      </main>
+    const navigation = useNavigation()
+    const isLoading = navigation.state === 'loading'
+    return (
+        <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+            {isLoading && <Loader />}
+            <Header />
+            {/* Here the outlet component is used to render component according to the url. So, according to /something -> The component specified in the element property of path: "/something" will be rendered by <Outlet/> component */}
+            <div className="overflow-y-auto">
+                <main className="mx-auto max-w-3xl">
+                    <Outlet />
+                </main>
+            </div>
 
-      <CartOverview />
-    </div>
-  );
+            <CartOverview />
+        </div>
+    )
 }
 
-export default AppLayout;
+export default AppLayout

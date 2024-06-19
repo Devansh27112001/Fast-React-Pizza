b49da077b5c924bd7d5cd56eef0dc74862b70201
Devansh27112001 <dkansara2711@gmail.com>
2024-06-19 09:33:17 -0400

    Styling the cart

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,16 +1,22 @@
-function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+import { formatCurrency } from '../../utilities/helpers';
+import Button from '../../ui/Button';
 
-  return (
-    <li>
-      <p>
-        {quantity}&times; {name}
-      </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
-      </div>
-    </li>
-  );
+function CartItem({ item }) {
+    const { pizzaId, name, quantity, totalPrice } = item;
+
+    return (
+        <li className="py-3 sm:flex sm:items-center sm:justify-between">
+            <p className="mb-1 sm:mb-0">
+                {quantity}&times; {name}
+            </p>
+            <div className="flex items-center sm:gap-6">
+                <p className="text-sm font-bold">
+                    {formatCurrency(totalPrice)}
+                </p>
+                <Button type="small">Delete</Button>
+            </div>
+        </li>
+    );
 }
 
 export default CartItem;

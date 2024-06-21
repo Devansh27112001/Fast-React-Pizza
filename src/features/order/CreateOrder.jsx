89d5602e3955c235250cbe70460f993ae310b5e2
Devansh27112001 <dkansara2711@gmail.com>
2024-06-21 00:34:25 -0400

    Usong the global --username state

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -34,6 +35,8 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+    const username = useSelector((state) => state.user.username);
+
     const navigation = useNavigation();
     const isSubmitting = navigation.state === 'submitting';
 
@@ -55,6 +58,7 @@ function CreateOrder() {
                         type="text"
                         name="customer"
                         className="input grow"
+                        defaultValue={username}
                         required
                     />
                 </div>
